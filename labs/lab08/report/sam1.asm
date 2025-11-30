bd1a7601170aa9b10405834667aa63bf1f13430b
Valeria Grigoreva <1032253494@pfur.ru>
2025-11-30 16:32:34 +0300

    feat(main): add files lab08

---
+++ labs/lab08/report/sam1.asm
@@ -0,0 +1,48 @@
+;-----------------------------------------------------------------
+; lab8-4.asm
+; Сумма значений f(x) = 6*x + 13 для аргументов командной строки
+;-----------------------------------------------------------------
+%include 'in_out.asm'
+
+SECTION .data
+msg_func    db 'Функция: f(x)=6*x+13',0h
+msg_res     db 'Результат: ',0h
+
+SECTION .text
+global _start
+_start:
+    ; Стек при входе (NASM): ... arg_n, ..., arg_1, argv0, argc
+    pop ecx          ; ecx := argc
+    pop edx          ; edx := argv0 (имя программы)
+    sub ecx, 1       ; ecx := кол-во реальных аргументов (без имени программы)
+
+    mov esi, 0       ; esi — накопитель суммы (32-bit)
+
+    cmp ecx, 0
+    jz .print_result
+
+.loop_args:
+    pop eax          ; eax := адрес очередного аргумента (строка)
+    push ecx         ; сохранить ecx (atoi/подпрограммы могут изменить ecx)
+    call atoi        ; преобразование строки (по адресу в eax) -> result в eax
+                     ; теперь eax = числовое значение x
+    pop ecx          ; восстановить ecx
+
+    imul eax, 6      ; eax = eax * 6
+    add eax, 13      ; eax = eax + 13  => eax = f(x)
+
+    add esi, eax     ; esi += f(x)
+
+    loop .loop_args  ; dec ecx; если !=0 -> переход к .loop_args
+
+.print_result:
+    mov eax, msg_func
+    call sprintLF
+
+    mov eax, msg_res
+    call sprint
+
+    mov eax, esi     ; результат в eax
+    call iprintLF    ; печать результата с переводом строки
+
+    call quit
